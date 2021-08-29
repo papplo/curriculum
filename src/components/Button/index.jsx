03061dee8f73d62aa23282b978faca10c85402a3
Pablo Anttila <pablo.anttila@gmail.com>
2021-08-29 16:40:34 +0200

    style: Button component gets revamped

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,9 @@
 import { h } from 'preact';
 import Styles from './styles.module.scss';
 
-function Button({ children }) {
-  return <span className={Styles.button}>{children}</span>;
+function Button({ children, props }) {
+  const getClassName = props?.inline ? `${Styles.button} ${Styles.inline}` : Styles.button;
+  return <span className={getClassName}>{children}</span>;
 }
 
 export default Button;
