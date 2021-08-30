12afebc9c720d3dfeb74a37852a28dffe9679761
Pablo Anttila <pablo.anttila@kabang.se>
2021-08-31 00:55:15 +0200

    config: create Constants for common strings within the app

---
+++ src/config.js
@@ -0,0 +1,25 @@
+export const SITE = {
+  title:
+    "Pablo Anttila — Frontend Developer specialized in JavaScript for Web and Mobile.",
+  description:
+    "Connect with Pablo Anttila, discover recent projects and blog posts. This portfolio site was build using Astro SSG and React deployed with CI tools to Github Pages.",
+  defaultLanguage: "se-SV",
+};
+
+export const KNOWN_LANGUAGES = {
+  sv: "Svenska",
+  en: "English",
+};
+
+export const MAIN_NAV = {
+  sv: [
+    { text: "Uppdrag", link: "/sv/assignments" },
+    { text: "Anteckningar", link: "/sv/devlog" },
+    { text: "Switch to English", link: "/en/" },
+  ],
+  en: [
+    { text: "Assignments", link: "/en/assignments" },
+    { text: "Devlog", link: "/en/devlog" },
+    { text: "På Svenska", link: "/sv/" },
+  ],
+};
