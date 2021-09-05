42b7e37c4226e2995d17f0d3b89ecf1c74da9b72
Pablo Anttila <pablo.anttila@gmail.com>
2021-09-05 18:58:28 +0200

    component: move aside markup into a separate component for dynamic content sitewide

---
+++ src/components/Common/Aside.jsx
@@ -0,0 +1,27 @@
+import { h } from 'preact';
+import Styles from './Aside.module.scss'
+
+const Aside = ({ image, bgColor, fgColor, caption, type = "default"}) => {
+    return (
+        <aside 
+            className={Styles.aside} 
+            data-tag={type} 
+            style={typeof backgroundColor === 'undefined' ? {
+                '--aside-bg-color': bgColor,
+            } : null}
+        >
+            {image && (
+                <figure className={Styles.figure}>
+                    <img src={image} />
+                    <figcaption
+                        style={typeof color === 'undefined' ? {
+                            '--theme-fg-color': fgColor,
+                        }: null}
+                    >{caption}</figcaption>
+                </figure>
+            )}
+        </aside>
+    )
+}
+
+export default Aside;
