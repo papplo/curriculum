3375a9ad486dba139c5e671e945f100d070335eb
Pablo Anttila <pablo@queenslab.se>
2022-12-29 23:25:07 +0100

    all paths fixed

---
+++ snowpack.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  alias: {
+    $components: "./src/components",
+    $common: ".src/components/common",
+    $layouts: "./src/layouts",
+    $assets: "./public/assets",
+    $scss: "./public/styles",
+    $: "./src",
+  },
+};
